25dd0d024ee8aaf882cde27a4be88d1884d63dc1
renx19 <138985568+renx19@users.noreply.github.com>
2025-12-13 00:36:59 +0800

    x

--- preschool-lms/api/index.php
+++ preschool-lms/api/index.php
@@ -1,14 +1,2 @@
 <?php
-// Load Laravel
-require __DIR__ . '/../vendor/autoload.php';
-
-// Boot Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
